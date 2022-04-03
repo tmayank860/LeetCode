b9baaf2ec9ffaa56730b02e395d8350bd2500ac6
MAYANK TYAGI <34006835+tmayank860@users.noreply.github.com>
2022-04-03 16:56:51 +0530

    Added solution - LeetHub

---
+++ Row with minimum number of 1's - GFG/row-with-minimum-number-of-1s.java
@@ -0,0 +1,51 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s[] = read.readLine().split(" ");
+            int N = Integer.parseInt(s[0]);
+            int M = Integer.parseInt(s[1]);
+            int A[][] = new int[N][M];
+            for (int i = 0; i < N; i++) {
+                String S[] = read.readLine().split(" ");
+                for (int j = 0; j < M; j++) {
+                    A[i][j] = Integer.parseInt(S[j]);
+                }
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.minRow(N, M, A));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int minRow(int N, int M, int A[][]) {
+        // code here
+        int res=0,current_count,min=Integer.MAX_VALUE;
+        for(int i=0;i<N;i++){
+            current_count=0;
+            for(int j=0;j<M;j++){
+                if(A[i][j]==1){
+                    current_count++;
+                }
+            }
+            if(current_count<min){
+                min=current_count;
+                res=i;
+            }
+        }
+        return res+1;
+    }
+}
