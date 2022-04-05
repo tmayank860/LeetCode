fc693f38ed1df34d54107dd16b48bc2b23b8ba7f
MAYANK TYAGI <34006835+tmayank860@users.noreply.github.com>
2022-04-05 15:09:57 +0530

    Time: 3 ms (57.07%), Space: 83.9 MB (38.19%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -11,5 +11,17 @@ class Solution {
            }
         }
         return profit;
+        
+        /*O(N^2) 
+          int profit=0;
+        for(int i=0;i<prices.length-1;i++){
+            for(int j=i+1;j<prices.length;j++){
+                if(prices[i]<prices[j]){
+                    profit=Math.max(profit,prices[j]-prices[i]);
+                }
+            }
+        }
+        return profit;
+        */
     }
 }
