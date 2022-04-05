f4f06e3057937ee046f8fd3943333bbae451676a
MAYANK TYAGI <34006835+tmayank860@users.noreply.github.com>
2022-04-05 16:22:35 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (97.93%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,7 +1,25 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n=matrix.length;
-        int r=0,c=n-1;
+        for(int i=0;i<n;i++){
+            for(int j=i;j<matrix[0].length;j++){
+                int temp=0;
+                temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+         }
+    
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+             int temp=0;
+                temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-j-1];
+                matrix[i][n-j-1]=temp;    
+            }
+         }
+        
+        /*int r=0,c=n-1;
         int[][] res=new int[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
@@ -15,6 +33,6 @@ class Solution {
             for(int j=0;j<n;j++){
                 matrix[i][j]=res[i][j];
             }
-         }
+         }*/
     }
 }
