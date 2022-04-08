dfef4ff3eff8f557d1f456c18a2cbc56401e0b97
MAYANK TYAGI <34006835+tmayank860@users.noreply.github.com>
2022-04-08 23:15:36 +0530

    Added solution - LeetHub

---
+++ Greater on right side - GFG/greater-on-right-side.java
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().nextGreatest(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+
+class Solution {
+    void nextGreatest(int arr[], int n) {
+        // code here
+        int max=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+            int current=arr[i];
+            arr[i]=max;
+            max=Math.max(current,max);
+        }
+        arr[n-1]=-1;
+    }
+}
