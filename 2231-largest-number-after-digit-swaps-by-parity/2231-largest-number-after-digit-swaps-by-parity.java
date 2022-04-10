ea29bc63f29bf9b9efaa6a2a51b559ffd7ec1e7e
MAYANK TYAGI <34006835+tmayank860@users.noreply.github.com>
2022-04-10 11:27:21 +0530

    Time: 8 ms (50.00%), Space: 41.3 MB (50.00%) - LeetHub

---
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int largestInteger(int num) {
+        PriorityQueue<Integer> even =new  PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> odd = new PriorityQueue<>(Collections.reverseOrder());
+        boolean flag=false;
+        int n=num,idx=0;
+        while(n!=0){
+            int temp=n%10;
+            if(temp%2==0){
+                even.add(temp);
+            }else{
+                odd.add(temp);
+            }
+            n=n/10;
+        }
+        String s=num+"";
+        flag=Integer.parseInt(s.charAt(0)+"")%2==0?true:false;
+        System.out.println(odd);
+         String res="";
+        // if(flag){
+        //     res+=even.poll()+"";
+        // }else{
+        //     res+=odd.poll()+"";
+        // }
+        // idx=1;
+        while(idx<s.length()){
+            if(Integer.parseInt(s.charAt(idx)+"")%2==0){
+                res+=even.poll()+"";
+            }else{
+                res+=odd.poll()+"";
+            }
+            idx++;
+        }
+        
+        return Integer.parseInt(res);
+    }
+}
