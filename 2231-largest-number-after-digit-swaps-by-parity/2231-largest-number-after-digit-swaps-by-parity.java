dab683661efea551f87ce422391b38d0fbc6abb9
MAYANK TYAGI <34006835+tmayank860@users.noreply.github.com>
2022-04-10 11:35:24 +0530

    Time: 3 ms (50.00%), Space: 39.3 MB (100.00%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.java
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.java
@@ -1,9 +1,11 @@
 class Solution {
     public int largestInteger(int num) {
+	//Priority Queue to split even odd values from the given number
         PriorityQueue<Integer> even =new  PriorityQueue<>(Collections.reverseOrder());
         PriorityQueue<Integer> odd = new PriorityQueue<>(Collections.reverseOrder());
         boolean flag=false;
         int n=num,idx=0;
+		//this while loop will add the numbers in priority queue
         while(n!=0){
             int temp=n%10;
             if(temp%2==0){
@@ -15,6 +17,7 @@ class Solution {
         }
         String s=num+"";
         String res="";
+		// here adding max(even or odd) number from priority queue to the result based on the number previously present at the same index
         while(idx<s.length()){
             if(Integer.parseInt(s.charAt(idx)+"")%2==0){
                 res+=even.poll()+"";
