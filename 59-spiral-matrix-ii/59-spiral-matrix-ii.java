68a2c0d2721b82ccce9b994cd38781926b138c9b
MAYANK TYAGI <34006835+tmayank860@users.noreply.github.com>
2022-04-13 10:07:16 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (82.25%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+        public int[][] generateMatrix(int n) {
+            int[][] matrix = new int[n][n];
+            int top = 0, left = 0, bottom = n - 1, right = n - 1, count = 1;
+
+            while(count <= n*n) {
+                for(int i=left; i<=right; i++) {
+                    matrix[top][i] = count++;
+                }
+                top++;
+                for(int i=top; i<=bottom; i++) {
+                    matrix[i][right] = count++;
+                }
+                right--;
+                for(int i=right; i>=left; i--) {
+                    matrix[bottom][i] = count++;
+                }
+                bottom--;
+                for(int i=bottom; i>=top; i--) {
+                    matrix[i][left] = count++;
+                }
+                left++;
+            }
+            return matrix;
+        }
+    }
