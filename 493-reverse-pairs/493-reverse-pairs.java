f9f42b4aa2d6b39768fbef180df12b3cb93d337a
MAYANK TYAGI <34006835+tmayank860@users.noreply.github.com>
2022-04-15 15:05:26 +0530

    Time: 106 ms (38.74%), Space: 50.2 MB (88.38%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.java
+++ 493-reverse-pairs/493-reverse-pairs.java
@@ -1,46 +1,43 @@
 class Solution {
-  static int merge(int[] nums, int low, int mid, int high) {
-        int cnt = 0;
-        int j = mid + 1; 
-        for(int i = low;i<=mid;i++) {
-            while(j<=high && nums[i] > (2 * (long) nums[j])) {
+  public static int reversePairs(int[] nums) {
+        return mergeSort(nums,0,nums.length-1);
+    }
+    static int merge(int[] arr, int low, int mid, int high) {
+        int count=0;
+        int j=mid+1;
+        for (int i=low;i<=mid;i++){
+            while (j<=high&&arr[i]>2*(long)arr[j]){
                 j++;
             }
-            cnt += (j - (mid+1));
+            count=count+(j-mid-1);
         }
-        
-        ArrayList<Integer> temp = new ArrayList<>(); 
-        int left = low, right = mid+1; 
-        while(left <= mid && right<=high) {
-            if(nums[left]<=nums[right]) {
-                temp.add(nums[left++]); 
-            }
-            else {
-                temp.add(nums[right++]); 
+        ArrayList<Integer> ls=new ArrayList<>();
+        int left=low,right=mid+1;
+        while (left<=mid&&right<=high){
+            if (arr[left]<=arr[right]){
+                ls.add(arr[left++]);
+            }else {
+                ls.add(arr[right++]);
             }
         }
-        
-        while(left<=mid) {
-            temp.add(nums[left++]); 
+        while (left<=mid){
+            ls.add(arr[left++]);
         }
-        while(right<=high) {
-            temp.add(nums[right++]); 
+        while (right<=high){
+            ls.add(arr[right++]);
         }
-        
-        for(int i = low; i<=high;i++) {
-            nums[i] = temp.get(i - low); 
+        for (int i=low;i<=high;i++){
+            arr[i]=ls.get(i-low);
         }
-        return cnt; 
+        return count;
     }
-    static int mergeSort(int[] nums, int low, int high) {
-        if(low>=high) return 0; 
-        int mid = (low + high) / 2;
-        int inv = mergeSort(nums, low, mid); 
-        inv += mergeSort(nums, mid+1, high); 
-        inv += merge(nums, low, mid, high); 
-        return inv; 
-    }
-   static int reversePairs(int[] nums) {
-        return mergeSort(nums, 0, nums.length - 1); 
+
+    static int mergeSort(int[] arr, int low, int high) {
+        if (low>=high)return 0;
+        int mid=(low+high)/2;
+        int interval=mergeSort(arr,low,mid);
+        interval+=mergeSort(arr,mid+1,high);
+        interval+=merge(arr,low,mid,high);
+        return interval;
     }
 }
