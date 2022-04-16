671ed9c5e9151df65402ed5c8243da08f223befb
MAYANK TYAGI <34006835+tmayank860@users.noreply.github.com>
2022-04-16 22:29:28 +0530

    Time: 19 ms (100.00%), Space: 39 MB (100.00%) - LeetHub

---
+++ 2240-number-of-ways-to-buy-pens-and-pencils/2240-number-of-ways-to-buy-pens-and-pencils.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long waysToBuyPensPencils(int total, int cost1, int cost2) {
+        if(cost1>total&&cost2>total){
+            return 1;
+        }
+        long sum=0;
+        int pencil=total/cost2;
+        int pen=0;
+        
+        while(pen*cost1<=total){
+            sum+=(total-(pen*cost1))/cost2+1;
+            pen++;
+        }
+        return sum;
+    }
+    
+}
