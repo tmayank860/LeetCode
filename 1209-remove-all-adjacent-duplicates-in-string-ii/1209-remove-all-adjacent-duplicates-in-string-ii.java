4b09ca37b845831484b7df6621f28d0e22ff8c10
MAYANK TYAGI <34006835+tmayank860@users.noreply.github.com>
2022-05-06 08:00:54 +0530

    Time: 7 ms (97.35%), Space: 49 MB (61.80%) - LeetHub

---
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String removeDuplicates(String s, int k) {
+          char[] arr = s.toCharArray();
+        int n = arr.length;
+        int[] count = new int[n];
+        int top = -1;
+        for (int i = 0; i < n; i++) {
+            if (top == -1) {
+                ++top;
+                arr[top] = arr[i];
+                count[top] = 1;
+            } else {
+                if (top >= 0 && arr[top] == arr[i] && (count[top] + 1) == k) {
+                    top -= k - 1;
+                } else {
+                    top++;
+                    count[top] = (arr[top - 1] == arr[i] ? (count[top - 1] + 1) : 1);
+                    arr[top] = arr[i];
+                }
+            }
+        }
+        return new String(arr, 0, top + 1);
+    }
+}
