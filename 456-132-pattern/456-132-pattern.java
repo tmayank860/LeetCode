11bf200c1e13b45e0027baa05da9a0a5d11de7a3
MAYANK TYAGI <34006835+tmayank860@users.noreply.github.com>
2022-05-11 20:03:49 +0530

    Time: 2 ms (99.87%), Space: 61.2 MB (86.88%) - LeetHub

---
+++ 456-132-pattern/456-132-pattern.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+
+        int second=Integer.MIN_VALUE;
+
+        int index=nums.length; //using nums itself as the stack (bottom on the right end)
+
+        for(int i=nums.length-1;i>=0;i--){
+
+            if(nums[i]<second) return true;
+
+            else while(index<nums.length && nums[i]>nums[index]) second=nums[index++];
+
+            nums[--index]=nums[i];
+
+        }
+
+        return false;
+    }
+}
