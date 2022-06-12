4cc74178433e45e89d0bb3fda3384d4a975ffe40
MAYANK TYAGI <34006835+tmayank860@users.noreply.github.com>
2022-06-12 14:03:06 +0530

    Time: 309 ms (5.04%), Space: 156.1 MB (5.04%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        HashMap<Integer,Integer> hm=new HashMap();
+        HashMap<Integer,Integer>loc=new HashMap<>(); //stores the index of last occurrance of the number
+        HashMap<Integer,Integer>sum=new HashMap<>(); //stores the prefix sum
+        int ans=0;//stores the final answer
+        int total=0;//stores the sum upto the index
+        int idx=-1;//stores the starting index from which the subarray has to be considered
+        for(int i=0;i<nums.length;i++)
+        {
+            total+=nums[i];//calculating total sum
+            if(loc.containsKey(nums[i])) //if the number repeats update the starting index of subarray
+                idx=Math.max(idx,loc.get(nums[i]));
+            if(idx==-1)
+                ans=Math.max(ans,total);
+            else
+                ans=Math.max(ans,total-sum.get(idx));//subtract prefix sum of starting of subarray from total to get sum of subarray
+            loc.put(nums[i],i);//update last occurance of the number
+            sum.put(i,total);//update prefix sum
+        }
+        return ans;
+    }
+}
